350c9453efbc594735984b68061f80f5aee329ac
Nguyễn Ngọc Tú <nguyenngoctu@Tusss.local>
2025-04-15 18:15:33 +0700

    second commit

---
+++ bookwebsite/src/layouts/Footer.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+function Footer (){
+    return (
+        <>
+        </>
+    )
+}
+
+export default Footer;
---
+++ bookwebsite/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function MainPage (){
+    return (
+        <>
+        <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default MainPage;
