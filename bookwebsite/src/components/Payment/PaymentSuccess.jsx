abffc059c3dc175ac23b334b1773e6b10f205420
Nguyen Anh Vu <hello@mail>
2025-04-24 22:17:39 +0700

    fix some payment function

--- bookwebsite/src/components/Payment/PaymentSuccess.jsx
+++ bookwebsite/src/components/Payment/PaymentSuccess.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/styles/PaymentSuccess.css';
-
+import { useNavigate } from 'react-router-dom';
 function PaymentSuccess() {
+  const navigate = useNavigate();
   return (
     <div className="container-fluid d-flex justify-content-center align-items-center vh-100 bg-light">
       <div className="success-box bg-success text-center p-4 rounded-4 shadow-lg position-relative">
@@ -15,7 +16,9 @@ function PaymentSuccess() {
           <h1 className="fw-bold mb-3">PAYMENT SUCCESSFUL!</h1>
           <p className="mb-0">THANK YOU FOR YOUR PURCHASE</p>
         </div>
-        <button className="btn btn-light fw-bold px-4 py-2 rounded-pill text-success shadow-sm">
+        <button className="btn btn-light fw-bold px-4 py-2 rounded-pill text-success shadow-sm" onClick={()=>{
+        navigate("/");
+      }}>
           CONTINUE
         </button>
       </div>
